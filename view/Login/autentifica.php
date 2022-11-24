51af8105bd232978c6882ad38cf93b37fc62dcca
Fran <fsanpaj255@g.educaand.es>
2022-11-24 18:41:34 +0100

    v6

--- view/Login/autentifica.php
+++ view/Login/autentifica.php
@@ -1,22 +1,36 @@
-
-   <!DOCTYPE html>
-   <html lang="es">
-   <head>
-       <meta charset="UTF-8">
-       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <title>Registro</title>
-       <link rel="stylesheet" href="/CSS/style.css">
-      
-   </head>
-   <body>
-       <form action="./controllers/controladorLogin.php" method="post" id="formulario" class="login">
-           <input type="text" name="correo" placeholder="Correo electrónico">      
-           <br>
-           <input type="password" name="contraseña" placeholder="Contraseña">       
-           <br>
-           <input type="submit" value="Iniciar sesión" name="inicioSesion"/>
-       </form>
-   </body>
-   </html>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<body>
+        
+        <div id="contenedor">
+            <div id="central">
+                <div id="login">
+                    <form id="loginform">
+                        <input type="text" name="correo" placeholder="Correo" required>
+                        
+                        <input type="password" placeholder="Contraseña" name="contrasena" required>
+                        
+                        <button type="submit" title="Ingresar" name="Ingresar">Iniciar sesión</button>
+                    </form>
+                    <div class="pie-form">
+                        <a href="#">¿Perdiste tu contraseña?</a>
+                        <a href="#">¿No tienes Cuenta? Registrate</a>
+                    </div>
+                </div>
+                <div class="inferior">
+                    <a href="?menu=index.php">Volver</a>
+                </div>
+            </div>
+        </div>
+            
+    </body>
+</body>
+</html>
+    
--- view/Principal/header.php
+++ view/Principal/header.php
@@ -1,8 +1,8 @@
 <header>
 <div class="topnav">
   <a class="active" href="?menu=default"><img src="source/ondas2.png"></a>
-  <a href="?menu=listadoparticipantes">Listado participantes</a>
-  <a href="?menu=listadoconcursos">Concursos</a>
-  <a href="?menu=autentifica"><button type="submit" href="?menu=autentifica">Iniciar sesión</button></a>
+  <a href="?menu=listadoparticipantes"><img class="logos" src="source/lista2.png">Listado participantes</a>
+  <a href="?menu=listadoconcursos"><img class="logos" src="source/trofeo.png">Concursos</a>
+  <a href="?menu=autentifica"><button type="submit" href="?menu=autentifica"><img class="logos" src="source/userLogin2.png">Iniciar sesión</button></a>
 </div>
 </header>  
